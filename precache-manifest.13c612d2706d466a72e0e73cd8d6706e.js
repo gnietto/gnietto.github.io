9803b4d3b52da47dbf0721a59ecbeee5d1128088
workstation <elcirculoti@gmail.com>
2020-08-01 23:12:47 -0400

    Updates

---
+++ precache-manifest.13c612d2706d466a72e0e73cd8d6706e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8cfe4db4c86945b9b1253c251167b8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0bae4a6ba3d510a677c8",
+    "url": "/static/js/2.404b70ed.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.404b70ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee88a6b03bb4a34420ba",
+    "url": "/static/js/main.62b9e79b.chunk.js"
+  },
+  {
+    "revision": "fff19337d3456cf73dd0",
+    "url": "/static/js/runtime-main.97d18319.js"
+  }
+]);
