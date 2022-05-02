7ab162544a4f64a5c2813fa68a6bcce0b2ab9fbe
workstation <elcirculoti@gmail.com>
2022-05-02 10:29:46 -0400

    Regresa a ReactDOM() de React v17

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const rootElement = document.getElementById('root');
+ReactDOM.render(
   <React.StrictMode>
     <ChakraProvider>
       <App />
     </ChakraProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  rootElement
 );
