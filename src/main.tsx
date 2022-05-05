855e9ab326ec5e5ec510e4cd9a8379e2866ed7d0
workstation <elcirculoti@gmail.com>
2022-05-05 19:42:07 -0400

    Implementa tipografía personalizada

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 import App from './App';
+import Fonts from './Fonts';
+import theme from './theme';
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
+      <Fonts />
       <App />
     </ChakraProvider>
   </React.StrictMode>,
