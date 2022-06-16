31080b5ff234c85e2163062d2147aafdceb44c41
gnietto <elcirculoti@gmail.com>
2022-06-16 12:34:38 -0400

    Cambia contenido a 1er micropost y agrega 2do micropost

--- src/posts.ts
+++ src/posts.ts
@@ -3,14 +3,22 @@ const posts = [
     key: 1,
     titulo: 'Despliegue Contínuo',
     texto: [
-      'Hasta hace muy poco, mis apps web eran desplegadas a través de un script npm que usa el paquete gh-pages. El paquete gh-pages es un buen programa, funciona bien y cumple su propósito: desplegar una app en Github Pages. Sin embargo, el tema del despliegue contínuo había estado rondando en mis ideas durante un buen tiempo, y la verdad es que ya sabía que era fácilmente factible de realizar. Así que hace unas dos semanas me avoqué a la tarea de implementar de una manera simple el flujo CI/CD. Por supuesto, la respuesta estaba muy cerca: Github Actions.',
-      'Github Actions permite implementar un script en formato .yml que usa los archivos de tu repositorio; procesa las tareas y especificaciones (entorno de sistema operativo, scripts definidos en tu bundler, repositorio de despliegue, entre muchas otras posibilidades) contenidos en el archivo .yml; y procede a desplegar tu app en Github Pages para que sea visible a todo el mundo.',
-      'Los beneficios que percibo de este nuevo esquema de despliegue son variados. Primero es que me permitió clarificar los pasos necesarios para que la plataforma Github pueda disponibilizar la app al público general. Segundo, me otorga comodidad en mi proceso de desarrollo puesto que ahora sólo necesito asegurarme que no existan errores en mi entorno local para poder pushear y tener certeza de que el flujo de Github Actions completará todos los pasos y desplegará la nueva versión de mi app. Tercero, puedo ver reflejados mis commits más rápido en el repositorio.',
-      'En síntesis, creo que el cambio de gh-pages a Github Actions ha sido todo ventajas, y me satisface haber implementado un nuevo flujo de trabajo y poder entender en principio cómo funciona. Mientras tanto, seguiré explorando las posibilidades de Github Actions.'
+      'Leyendo acerca de Ingeniería de Software, me entusiasmé y decidí implementar CI/CD en mi flujo de desarrollo via Github Actions. Después de adaptar el script a mi flujo, simplemente I loved it. Por ahora mi implementación es CD a secas, pronto agregaré el factor CI a la ecuación.'
     ],
     autor: 'Germán',
-    fecha: 'Diciembre 30, 2021',
+    fecha: 'Junio 15, 2022',
     link: '/cd',
+    tag: 'Devops'
+  },
+  {
+    key: 2,
+    titulo: 'De Windows a Linux',
+    texto: [
+      'Durante mucho tiempo he usado Windows 10 para todo asunto digital. Funciona bien, pero ocupa demasiada RAM en asuntos del sistema en vez de asuntos productivos míos. Así que decidí por 3° vez en mi vida cambiar de SO. Esta vez probé OpenBSD y luego Linux. La verdad estoy bastante conforme.'
+    ],
+    autor: 'Germán',
+    fecha: 'Junio 16, 2022',
+    link: '/windows-linux',
     tag: 'Devops'
   }
 ];
